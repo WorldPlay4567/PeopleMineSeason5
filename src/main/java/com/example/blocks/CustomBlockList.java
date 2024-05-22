fd57316a2460653ec89599c462695a14717a12e1
WorldPlay4567 <vladsekotizin820@gmail.com>
2024-05-22 15:31:36 +0300

    Не удавшийся експеримент

--- src/main/java/com/example/blocks/CustomBlockList.java
+++ src/main/java/com/example/blocks/CustomBlockList.java
@@ -1,20 +1,14 @@
 package com.example.blocks;
 
-import com.example.PeopleMineSeason5;
-import com.example.items.BluePrint;
 import eu.pb4.polymer.blocks.api.BlockModelType;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.block.MapColor;
-import net.minecraft.block.enums.Instrument;
-import net.minecraft.item.Item;
-import net.minecraft.item.Items;
 
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
-import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.Rarity;
 
 import static net.minecraft.registry.Registries.BLOCK;
 
@@ -22,17 +16,20 @@ public class CustomBlockList {
 
 //    public static final Block CustomBlockTest = Blocks.register(String.valueOf(Identifier.of(PeopleMineSeason5.MOD_ID, "custom_block_test"));
 
-    public static void register(BlockModelType type, String modelId) {
+    public static Block register(BlockModelType type, String modelId) {
         var id = new Identifier("peoplemineseason5", modelId);
         var block = Registry.register(BLOCK, id,
                 new CustomBlockTest(FabricBlockSettings.copy(Blocks.DIAMOND_BLOCK), type, modelId));
 
+
+        Registry.register(Registries.ITEM, id, new CustomBlockItem(new Item.Settings(), block, modelId));
+        return block;
     }
 
-
-
+    public static final Block CUSTOM_BLOCK_ENTITY = register(BlockModelType.TRANSPARENT_BLOCK, "block/custom_block_test");
 
     public static void init() {
-        register(BlockModelType.TRANSPARENT_BLOCK, "block/custom_block_test");
+
+
     }
 }
--- src/main/java/com/example/blocks/CustomBlockTest.java
+++ src/main/java/com/example/blocks/CustomBlockTest.java
@@ -1,34 +1,36 @@
 package com.example.blocks;
 
-import com.example.PeopleMineSeason5;
 import com.example.gui.PMGui;
 import eu.pb4.polymer.blocks.api.BlockModelType;
 import eu.pb4.polymer.blocks.api.PolymerBlockModel;
 import eu.pb4.polymer.blocks.api.PolymerBlockResourceUtils;
 import eu.pb4.polymer.blocks.api.PolymerTexturedBlock;
-import eu.pb4.polymer.core.api.block.PolymerBlock;
-import eu.pb4.polymer.core.api.utils.PolymerClientDecoded;
-import eu.pb4.polymer.core.api.utils.PolymerKeepModel;
-import eu.pb4.polymer.resourcepack.api.PolymerModelData;
-import eu.pb4.polymer.resourcepack.api.PolymerResourcePackUtils;
+import eu.pb4.polymer.virtualentity.api.ElementHolder;
+import eu.pb4.polymer.virtualentity.api.attachment.ChunkAttachment;
+import eu.pb4.polymer.virtualentity.api.elements.BlockDisplayElement;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.entity.decoration.DisplayEntity;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
+
 
 public class CustomBlockTest extends Block implements PolymerTexturedBlock {
     private final BlockState polymerBlockState;
 
+    SpawnBlock spawnBlock = new SpawnBlock();
+
+    Random random = Random.create();
     public CustomBlockTest(Settings settings, BlockModelType type, String modelId) {
         super(settings);
 
@@ -36,13 +38,77 @@ public class CustomBlockTest extends Block implements PolymerTexturedBlock {
                 type,
                 PolymerBlockModel.of(new Identifier("peoplemineseason5", modelId)));
 
+
+    }
+
+    @Override
+    public BlockState onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        spawnBlock.destroy();
+        return state;
+    }
+
+    @Override
+    public ActionResult onUse(BlockState state, World world, BlockPos pos, PlayerEntity player, BlockHitResult hit) {
+        PMGui.open((ServerPlayerEntity) player);
+        return ActionResult.success(true);
+    }
+
+    @Override
+    public void onPlaced(World world, BlockPos pos, BlockState state, @Nullable LivingEntity placer, ItemStack itemStack) {
+        spawnBlock.onPlace(world,pos);
     }
 
 
-    
+//    @Override
+//    public void scheduledTick(BlockState state, ServerWorld world, BlockPos pos, Random random) {
+//        double d = (double)pos.getX() + 0.55 - (double)(random.nextFloat() * 0.1F);
+//        double e = (double)pos.getY() + 0.55 - (double)(random.nextFloat() * 0.1F);
+//        double f = (double)pos.getZ() + 0.55 - (double)(random.nextFloat() * 0.1F);
+//        if (random.nextInt(5) == 0) {
+//            ServerWorld serverWorld = world.getServer().getOverworld();
+//            serverWorld.spawnParticles(ParticleTypes.LARGE_SMOKE, d, e, f,1, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005, random.nextGaussian() * 0.005, 0.01);
+//        }
+//        System.out.println(d);
+//        System.out.println(e);
+//        System.out.println(f);
+//        super.scheduledTick(state, world, pos, random);
+//    }
+
     @Override
     public BlockState getPolymerBlockState(BlockState state) {
         return this.polymerBlockState;
 
     }
+
+    public static final class SpawnBlock {
+        private static ElementHolder holder;
+
+        public SpawnBlock (){
+
+        }
+
+        public static void onPlace(World world, BlockPos pos){
+            holder = new ElementHolder(){
+                @Override
+                public void tick() {
+                    super.tick();
+                }
+            };
+
+            var blockPos = new BlockPos(pos.getX(), pos.getY(), pos.getZ());
+            new ChunkAttachment(holder, world.getWorldChunk(blockPos), pos.toCenterPos(), true);
+            BlockDisplayElement blockElement = new BlockDisplayElement(Blocks.DIAMOND_BLOCK.getDefaultState());
+            holder.addElement(blockElement);
+            System.out.println("MiddleGenerator created" + pos);
+        }
+
+        public static void destroy() {
+            holder.destroy();
+        }
+
+
+
+
+    }
+
 }
