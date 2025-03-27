04a0a382315c7862d620e526cfdcf2be35581b5d
WorldPlay4567 <58886507+WorldPlay4567@users.noreply.github.com>
2025-03-27 12:00:14 +0200

    fix3

--- src/main/java/com/worldplay/mixin/VillagerMixin.java
+++ src/main/java/com/worldplay/mixin/VillagerMixin.java
@@ -42,81 +42,80 @@ public abstract class VillagerMixin {
 //			test7((ServerPlayerEntity) player, ConfigVillagerRegister.STONE);
 			cir.setReturnValue(ActionResult.FAIL);
 		}
-		if (villager.hasCustomName() && "Фермер".equals(villager.getCustomName().getString())) {
-			test7((ServerPlayerEntity) player, ConfigVillagerRegister.PRODUCT);
-			cir.setReturnValue(ActionResult.FAIL);
-		}
+//		if (villager.hasCustomName() && "Фермер".equals(villager.getCustomName().getString())) {
+//			test7((ServerPlayerEntity) player, ConfigVillagerRegister.PRODUCT);
+//			cir.setReturnValue(ActionResult.FAIL);
+//		}
 		if (villager.hasCustomName() && "Строитель".equals(villager.getCustomName().getString())) {
 			new VillagerBluePrint((ServerPlayerEntity) player);
 		}
 		// This code is injected into the start of MinecraftServer.loadWorld()V
 	}
-
-
-	@Unique
-	private static void test7(ServerPlayerEntity player, ConfigVillager configVillager) {
-		try {
-			MerchantGui gui = new MerchantGui(player, false) {
-
-				@Override
-				public void onSelectTrade(TradeOffer offer) {
-					this.player.sendMessage(Text.literal("Selected Trade: " + this.getOfferIndex(offer)), false);
-				}
-
-				@Override
-				public void onSuggestSell(TradeOffer offer) {
-					if (offer != null && offer.getSellItem() != null) {
-						this.sendUpdate();
-					}
-
-				}
-
-				@Override
-				public void setExperience(int experience) {
-					System.out.println("exp : " + experience);
-				}
-
-				@Override
-				public boolean onTrade(TradeOffer offer) {
-					System.out.println("BUY : " + this.getExperience());
-					int i = this.getOfferIndex(offer);
-//					ConfigVillager.setBuyMoney(i, "stone");
-					configVillager.setBuyMoney(i);
-					this.sendUpdate();
-					return true;
-				}
-
-
-				@Override
-				public void onClose() {
-					super.onClose();
-					System.out.println("TEST EXP GET : " + this.getExperience());
-				}
-			};
-
-			gui.setTitle(Text.literal("Продавец камня"));
-			gui.setIsLeveled(true);
-
-
-//			gui.addTrade(new TradeOffer(
-//					new TradedItem(Items.EMERALD, 1),
-//					new GuiElementBuilder(Items.ANDESITE)
-//							.glow()
-//							.setCount(16)
-//							.asStack(),
-//					1,
-//					1,
-//					1
-//			));
-
-//			ConfigVillager.getTrader("stone", gui);
-			configVillager.getTrader(gui);
-
-			gui.open();
-
-
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+	
+//	@Unique
+//	private static void test7(ServerPlayerEntity player, ConfigVillager configVillager) {
+//		try {
+//			MerchantGui gui = new MerchantGui(player, false) {
+//
+//				@Override
+//				public void onSelectTrade(TradeOffer offer) {
+//					this.player.sendMessage(Text.literal("Selected Trade: " + this.getOfferIndex(offer)), false);
+//				}
+//
+//				@Override
+//				public void onSuggestSell(TradeOffer offer) {
+//					if (offer != null && offer.getSellItem() != null) {
+//						this.sendUpdate();
+//					}
+//
+//				}
+//
+//				@Override
+//				public void setExperience(int experience) {
+//					System.out.println("exp : " + experience);
+//				}
+//
+//				@Override
+//				public boolean onTrade(TradeOffer offer) {
+//					System.out.println("BUY : " + this.getExperience());
+//					int i = this.getOfferIndex(offer);
+////					ConfigVillager.setBuyMoney(i, "stone");
+//					configVillager.setBuyMoney(i);
+//					this.sendUpdate();
+//					return true;
+//				}
+//
+//
+//				@Override
+//				public void onClose() {
+//					super.onClose();
+//					System.out.println("TEST EXP GET : " + this.getExperience());
+//				}
+//			};
+//
+//			gui.setTitle(Text.literal("Продавец камня"));
+//			gui.setIsLeveled(true);
+//
+//
+////			gui.addTrade(new TradeOffer(
+////					new TradedItem(Items.EMERALD, 1),
+////					new GuiElementBuilder(Items.ANDESITE)
+////							.glow()
+////							.setCount(16)
+////							.asStack(),
+////					1,
+////					1,
+////					1
+////			));
+//
+////			ConfigVillager.getTrader("stone", gui);
+//			configVillager.getTrader(gui);
+//
+//			gui.open();
+//
+//
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//	}
 }
